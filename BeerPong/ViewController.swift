7c634ad6c12edcce9c157a7afa879b89e5b1b07c
seanohare98 <seanohare@link.cuhk.edu.hk>
2020-11-06 18:28:17 +0800

    flat surface detection, 3d obj anchoring, playing around

--- BeerPong/ViewController.swift
+++ BeerPong/ViewController.swift
@@ -13,30 +13,74 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    
+    var planes = [ARPlaneAnchor: Plane]()
+    var visibleGrid: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
+        // Set debugging tools
         sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        sceneView.addGestureRecognizer(gestureRecognizer)
+        
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.planeDetection = .horizontal
+        
+        UIApplication.shared.isIdleTimerDisabled = true
+        sceneView.autoenablesDefaultLighting = true
+        
         // Run the view's session
         sceneView.session.run(configuration)
+        
+
+    }
+    
+    @objc func tapped(gesture: UITapGestureRecognizer) {
+        // Get exact position where touch happened on screen of iPhone (2D coordinate)
+        let touchPosition = gesture.location(in: sceneView)
+        
+        let hitTestResult = sceneView.hitTest(touchPosition, types: .existingPlaneUsingExtent)
+
+        if !hitTestResult.isEmpty {
+            guard let hitResult = hitTestResult.first else {
+                return
+            }
+
+            addCups(hitTestResult: hitResult)
+        }
+    }
+    
+    
+    func addCups(hitTestResult: ARHitTestResult) {
+
+        let cup = SCNCylinder(radius: 0.05, height: 0.2)
+        let cupNode = SCNNode()
+        let cupMaterial = SCNMaterial()
+        cupMaterial.diffuse.contents = UIColor.red
+        cupNode.geometry = cup
+        cupNode.geometry?.materials = [cupMaterial]
+        cupNode.position = SCNVector3(hitTestResult.worldTransform.columns.3.x,hitTestResult.worldTransform.columns.3.y, hitTestResult.worldTransform.columns.3.z)
+    
+          sceneView.scene.rootNode.addChildNode(cupNode)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -46,16 +90,36 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+           guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+            self.addPlane(node: node, anchor: planeAnchor)
     }
-*/
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        self.updatePlane(anchor: planeAnchor)
+
+    }
+
+    func addPlane(node: SCNNode, anchor: ARPlaneAnchor) {
+        let plane = Plane(anchor)
+        planes[anchor] = plane
+        node.addChildNode(plane)
+    }
+    
+    func updatePlane(anchor: ARPlaneAnchor) {
+        if let plane = planes[anchor] {
+            plane.update(anchor)
+        }
+    }
+
+    func removePlane(anchor: ARPlaneAnchor) {
+        if let plane = planes.removeValue(forKey: anchor) {
+            plane.removeFromParentNode()
+        }
+    }
+    
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
